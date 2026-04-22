85f4a4f49239f4125b06b881e24543dca70e7770
AdrianG <arajeev@gesco.com>
2026-04-21 17:07:07 -0700

    fix(config): add missing Microsoft.Extensions.Configuration using
    - DbMigrator uses .ConfigureAppConfiguration((ctx, cfg) => ...) where
      cfg is IConfigurationBuilder; AddJsonFile on IConfigurationBuilder
      is an extension method in Microsoft.Extensions.Configuration.Json
      (surfaced via the parent namespace on modern SDKs).
    - The other two services (AuthServer, HttpApi.Host) call AddJsonFile
      on builder.Configuration which is ConfigurationManager (instance
      method), so no using change needed there.

--- src/HealthcareSupport.CaseEvaluation.DbMigrator/Program.cs
+++ src/HealthcareSupport.CaseEvaluation.DbMigrator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
