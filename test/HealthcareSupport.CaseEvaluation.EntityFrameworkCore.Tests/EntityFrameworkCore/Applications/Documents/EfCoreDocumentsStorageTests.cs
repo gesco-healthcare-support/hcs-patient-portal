30b3d05549b56e67f871537d9066193ba76cd7dc
Adrian <adriang@gesco.com>
2026-09-24 11:47:49 -0700

    test(test-harness): drop the shared collection from #1042's classes
    - #1042 merged after this branch was cut and brought 4 EF Core classes
      that still named the removed serial collection
    - Same mechanical edit as the rest: the attribute line and the then
      unused using Xunit directive
    
    Part of #1034

--- test/HealthcareSupport.CaseEvaluation.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Documents/EfCoreDocumentsStorageTests.cs
+++ test/HealthcareSupport.CaseEvaluation.EntityFrameworkCore.Tests/EntityFrameworkCore/Applications/Documents/EfCoreDocumentsStorageTests.cs
@@ -1,9 +1,7 @@
 using HealthcareSupport.CaseEvaluation.EntityFrameworkCore;
-using Xunit;
 
 namespace HealthcareSupport.CaseEvaluation.Documents;
 
-[Collection(CaseEvaluationTestConsts.CollectionDefinitionName)]
 public class EfCoreDocumentsStorageTests
     : DocumentsStorageTests<CaseEvaluationEntityFrameworkCoreTestModule>
 {
